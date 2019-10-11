83103c7108d9482705f6e45d61347e1baef98d4c
Ian Miller <ian_c_miller@icloud.com>
2019-10-11 19:30:31 -0400

    removed everything in getPredictions except console.log

--- routes/getPredictions.js
+++ routes/getPredictions.js
@@ -1,12 +1,12 @@
 module.exports = (positionId) => {
-    const { spawnSync } = require('child_process')
-    let predictions = spawnSync('/usr/bin/python3', ['sorting_script.py',positionId], {
-      stdio: 'pipe',
-      encoding: 'utf-8' 
-    })
-    console.log("predictions",  predictions)
+    // const { spawnSync } = require('child_process')
+    // let predictions = spawnSync('/usr/bin/python3', ['sorting_script.py',positionId], {
+    //   stdio: 'pipe',
+    //   encoding: 'utf-8' 
+    // })
+    console.log("predictions",  positionId)
     
-    return predictions.output[1]
+    // return predictions.output[1]
 }
 
 //  getPredictions
