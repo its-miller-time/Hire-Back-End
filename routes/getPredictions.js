e099811cf6dadf5ae022bbceaf9e56380f2cbadf
Ian Miller <ian_c_miller@icloud.com>
2019-10-11 21:12:21 -0400

    uncommenting getPredictions

--- routes/getPredictions.js
+++ routes/getPredictions.js
@@ -1,12 +1,10 @@
 module.exports = (positionId) => {
-    // const { spawnSync } = require('child_process')
-    // let predictions = spawnSync('/usr/bin/python3', ['sorting_script.py',positionId], {
-    //   stdio: 'pipe',
-    //   encoding: 'utf-8' 
-    // })
-    console.log("predictions",  positionId)
-    
-    // return predictions.output[1]
+    const { spawnSync } = require('child_process')
+    let predictions = spawnSync('/usr/bin/python3', ['sorting_script.py',positionId], {
+      stdio: 'pipe',
+      encoding: 'utf-8' 
+    })    
+    return predictions.output[1]
 }
 
 //  getPredictions
