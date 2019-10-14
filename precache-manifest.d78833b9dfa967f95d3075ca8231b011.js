4b1e4447b83f53517b2af9306d039f3a6130e256
Ian Miller <ian_c_miller@icloud.com>
2019-10-14 11:17:14 -0400

    adding build of style updates

---
+++ precache-manifest.d78833b9dfa967f95d3075ca8231b011.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7521ead6d52a8c50a33117dc76bd8323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0adf016e540cb8bdd885",
+    "url": "/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "c1e06e9107ef3ed5fb7b",
+    "url": "/static/css/main.1bc7ecb4.chunk.css"
+  },
+  {
+    "revision": "0adf016e540cb8bdd885",
+    "url": "/static/js/2.d2e6468c.chunk.js"
+  },
+  {
+    "revision": "c1e06e9107ef3ed5fb7b",
+    "url": "/static/js/main.45ab1cb4.chunk.js"
+  },
+  {
+    "revision": "b16b46bb4f1b922edce6",
+    "url": "/static/js/runtime~main.8b832b20.js"
+  },
+  {
+    "revision": "1890f5a0820208813bb84395fbf9f516",
+    "url": "/static/media/calendar.1890f5a0.svg"
+  },
+  {
+    "revision": "42f4e5d8bc6853574f544d07c8ab2561",
+    "url": "/static/media/id-card.42f4e5d8.svg"
+  },
+  {
+    "revision": "d7285f3e33b1b28f2b1906148998a115",
+    "url": "/static/media/money.d7285f3e.svg"
+  },
+  {
+    "revision": "1c1c3f33e4b643133d3ba20521bc3740",
+    "url": "/static/media/report.1c1c3f33.svg"
+  },
+  {
+    "revision": "cd13e935f696173ef3b9e6be9d04ac45",
+    "url": "/static/media/superhero.cd13e935.svg"
+  },
+  {
+    "revision": "15679d32a705ceb3e194c64611409539",
+    "url": "/static/media/tools.15679d32.svg"
+  }
+]);
--- routes/getPredictions.js
+++ routes/getPredictions.js
@@ -1,9 +1,10 @@
 module.exports = (positionId) => {
     const { spawnSync } = require('child_process')
-    let predictions = spawnSync('/usr/bin/python3', ['sorting_script.py',positionId], {
+    let predictions = spawnSync('python3', ['sorting_script.py',positionId], {
       stdio: 'pipe',
       encoding: 'utf-8' 
     })    
+    console.log(predictions)
     console.log('getPredictions fired')
     return predictions.output[1]
 }
