847b45abe79baed5fad4d3e630ef10dc7909a655
Ian Miller <ian_c_miller@icloud.com>
2019-10-14 18:45:40 -0400

    fixing script route

--- routes/getPredictions.js
+++ routes/getPredictions.js
@@ -1,11 +1,9 @@
 module.exports = (positionId) => {
     const { spawnSync } = require('child_process')
-    let predictions = spawnSync('python3', ['sorting_script.py',positionId], {
+    let predictions = spawnSync('python3', ['../sorting_script.py',positionId], {
       stdio: 'pipe',
       encoding: 'utf-8' 
     })    
-    console.log(predictions)
-    console.log('getPredictions fired')
     return predictions.output[1]
 }
 
